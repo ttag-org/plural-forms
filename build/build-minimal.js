6036524c9abe4b3f8e1e6aac5cbe6a6ecdfb2388
a.mostovenko <a.mostovenko@smartweb.com.ua>
2017-11-16 18:29:25 +0200

    nice start

---
+++ build/build-minimal.js
@@ -0,0 +1,33 @@
+const catalogData = require('../src/catalog');
+const pluralRegex = /\splural ?=?([\s\S]*);?/;
+
+function getPluralFunc(pluralsText) {
+    try {
+        let pluralFn = pluralRegex.exec(pluralsText)[1];
+        if (pluralFn[pluralFn.length - 1] === ';') {
+            pluralFn = pluralFn.slice(0, -1);
+        }
+        return pluralFn;
+    } catch (err) {
+        throw new Error(`Failed to parse plural func from headers "${pluralsText}"\n`);
+    }
+}
+
+function getNPlurals(pluralsText) {
+    const nplurals = /nplurals ?= ?(\d)/.exec(pluralsText)[1];
+    return parseInt(nplurals, 10);
+}
+
+const minimalData = {};
+
+Object.keys(catalogData).forEach((key) => {
+    const value = catalogData[key];
+    const nPlurals = getNPlurals(value.pluralsText);
+    const formula = getPluralFunc(value.pluralsText)
+        .replace(/\s/g, '')
+        .replace(/^\(/, '')
+        .replace(/\)$/, '');
+    minimalData[key] = `${nPlurals};${formula}`;
+});
+
+console.log(JSON.stringify(minimalData));
