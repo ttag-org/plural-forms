696905b482eeafb097f8cc71e728bc246b33721d
a.mostovenko <a.mostovenko@smartweb.com.ua>
2017-11-17 17:54:56 +0200

    added ts definitions

---
+++ plural-forms.d.ts
@@ -0,0 +1,6 @@
+export declare function getFormula(locale: string): string;
+export declare function getNPlurals(locale: string): string;
+export declare function getPluralFunc(locale: string): string;
+export declare function hasLocale(locale: string): boolean; 
+export function printAvailableLocales(silent:boolean = false): string;
+export declare function getPluralFormsHeader(locale: string): string;
