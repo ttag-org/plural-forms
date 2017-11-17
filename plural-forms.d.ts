0e7873c76fd08d7d286a3a01574065834c3d1aa2
a.mostovenko <a.mostovenko@smartweb.com.ua>
2017-11-17 18:00:46 +0200

    fix def

--- plural-forms.d.ts
+++ plural-forms.d.ts
@@ -2,5 +2,5 @@ export declare function getFormula(locale: string): string;
 export declare function getNPlurals(locale: string): string;
 export declare function getPluralFunc(locale: string): string;
 export declare function hasLocale(locale: string): boolean; 
-export function printAvailableLocales(silent:boolean = false): string;
+export function printAvailableLocales(silent:boolean): string;
 export declare function getPluralFormsHeader(locale: string): string;
