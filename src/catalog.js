22d0a12bd6ad141f95eca0f16660d140f217471b
mmso <mmso@mmso.se>
2020-10-30 12:55:43 +0100

    Add kabyle support

--- src/catalog.js
+++ src/catalog.js
@@ -905,6 +905,21 @@ export default {
             return 0;
         }
     },
+    kab: {
+        name: 'Kabyle',
+        examples: [{
+            plural: 0,
+            sample: 1
+        }, {
+            plural: 1,
+            sample: 2
+        }],
+        nplurals: 2,
+        pluralsText: 'nplurals = 2; plural = (n != 1)',
+        pluralsFunc: function(n) {
+            return (n !== 1);
+        }
+    },
     kk: {
         name: 'Kazakh',
         examples: [{
